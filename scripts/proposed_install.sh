4ada2690405d737e6766220c876a5e1fcd48583d
Ponnuvel Palaniyappan <pponnuvel@gmail.com>
2020-11-09 16:33:49 +0000

    Update proposed_install.sh

--- scripts/proposed_install.sh
+++ scripts/proposed_install.sh
@@ -2,5 +2,5 @@
 
 sudo sh -c "echo 'deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -cs)-proposed restricted main multiverse universe' >> /etc/apt/sources.list.d/proposed-repositories.list"
 sudo apt update
-sudo apt -t $(lsb_release -cs) -proposed install some-package
+sudo apt -t $(lsb_release -cs)-proposed install some-package
 
